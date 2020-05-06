904a208b6be6a4288030e4944a7614a0b8c40e2b
void-spark <81029971+void-spark@users.noreply.github.com>
2020-05-06 20:04:25 +0100

    Remove no longer used files

--- git_backup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd ~/node-red-config
-docker cp nodered:/data/flows.json -| tar -xO | python -m json.tool > flows_raspberrypi.json
-git commit -am "Auto commit `date -Iminutes`"
-git push -u origin master
--- update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker build --no-cache --pull -t node-red-docker . &&
-docker stop nodered &&
-docker rm nodered &&
-docker run --name nodered -dit --restart unless-stopped -p 1880:1880 node-red-docker
